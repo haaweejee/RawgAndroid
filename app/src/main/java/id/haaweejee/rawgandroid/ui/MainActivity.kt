d46dd784b27708201f2de9f166c9b269443610e1
Hendrawan Wibowo Jatmiko <52823787+haaweejee@users.noreply.github.com>
2023-08-17 14:43:14 +0700

    + init mainactivity

---
+++ app/src/main/java/id/haaweejee/rawgandroid/ui/MainActivity.kt
@@ -0,0 +1,30 @@
+package id.haaweejee.rawgandroid.ui
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import dagger.hilt.android.AndroidEntryPoint
+import id.haaweejee.rawgandroid.ui.theme.RawgAndroidTheme
+
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            RawgAndroidTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background,
+                ) {
+                    RawgApp()
+                }
+            }
+        }
+    }
+}
